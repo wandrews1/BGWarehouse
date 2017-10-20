bf877292221c8991de3d5d507dbef35473fec2f0
Billy Andrews <scripture187@gmail.com>
2017-10-20 12:55:25 +0000

    Added JHuff and BKert

--- bg.sql
+++ bg.sql
@@ -19,6 +19,8 @@ CREATE TABLE login (
   );
 
 INSERT INTO login (email, fname, lname, pw1, address, city, state, zipcode) VALUES ('scripture187@gmail.com','Billy', 'Andrews', crypt('pass', gen_salt('bf')), '78 C L Walker Blvd', 'Fredericksburg', 'VA', '22407');
+INSERT INTO login (email, fname, lname, pw1, address, city, state, zipcode) VALUES ('bkertche@umw.com','Brendon', 'Kertcher', crypt('pass', gen_salt('bf')), 'Somewhere', 'Fredericksburg', 'VA', '22407');
+INSERT INTO login (email, fname, lname, pw1, address, city, state, zipcode) VALUES ('jhuffma3@umw.com','Jacob', 'Huffman', crypt('pass', gen_salt('bf')), 'Somewhere', 'Fredericksburg', 'VA', '22407');
 
 
 
